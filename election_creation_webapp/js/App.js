6d2db74be54219af6b82e3b2be3ce3e135a234e7
Elijah Jasso <elijahjasso19@gmail.com>
2021-04-20 22:29:58 -0700

    more comments

--- election_creation_webapp/js/App.js
+++ election_creation_webapp/js/App.js
@@ -5,33 +5,41 @@ class App {
         this.electionCreatorContract = null;
     }
 
+    // Connect to MetMask to connect to user's address to interact/deploy smart contract
     async connectMetamask() {
         let connectedAccount = await this.processor.connectMetamask();
         if (connectedAccount == null) {
+            alert('Could not conenct to your MetaMask account');
             return;
         }
 
+        // change the UI to reflect result
         this.ui.setupConnectToElectionCreator(connectedAccount);
     }
 
+    // Setup the ElectionCreatorHandler
     async connectToElectionCreator() {
-        this.ui.startSpinner();
+        this.ui.startSpinner(); // UI spinner start
         this.electionCreatorContract = new ElectionCreatorHandler(this.ui.getElectionCreatorAddress(), this.processor.web3);
         await this.electionCreatorContract.connect();
-        this.ui.stopSpinner();
+        this.ui.stopSpinner();  // UI spinner stop
         console.log('connected to election creator');
 
+        // change UI
         this.ui.setupElectionParams();
     }
 
+
+    // create an election
     async createElection() {
         this.ui.startSpinner();
-        let candidates = this.processor.seperateCommas(this.ui.getCandidates());
-        let voters = this.processor.seperateCommas(this.ui.getVoters());
-        let electionName = this.ui.getElectionName();
+        let candidates = this.processor.seperateCommas(this.ui.getCandidates()); // get candidates from UI and clean them
+        let voters = this.processor.seperateCommas(this.ui.getVoters()); // get voters from UI and clean them
+        let electionName = this.ui.getElectionName(); // get election name
         let electionAddress = await this.electionCreatorContract.create(electionName, candidates, 
-                voters, this.processor.account);
-        let [p, g] = this.electionCreatorContract.getPandG();
+                voters, this.processor.account); // create election and get election address
+        let [p, g] = this.electionCreatorContract.getPandG(); // get prime p and generator g
+        // update UI
         this.ui.showCompleted(electionName, candidates, voters, p, g, electionAddress);
 
         this.ui.stopSpinner();
--- election_creation_webapp/js/backend/ElectionCreatorHandler.js
+++ election_creation_webapp/js/backend/ElectionCreatorHandler.js
@@ -1,18 +1,21 @@
 class ElectionCreatorHandler {
     constructor(address, web3) {
+        // address of account connected via MetaMask
         this.address = address;
+        // web3 instance
         this.web3 = web3;
+        // abi, application binary interface to let MetaMask know the layout of the smart contract
         this.abi = JSON.parse('[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"string","name":"_electionName","type":"string"},{"internalType":"string[]","name":"_candidates","type":"string[]"},{"internalType":"address[]","name":"_voters","type":"address[]"},{"internalType":"bytes","name":"_p","type":"bytes"},{"internalType":"bytes","name":"_g","type":"bytes"}],"name":"createElection","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"getAllElections","outputs":[{"internalType":"string[]","name":"","type":"string[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"_electionName","type":"string"}],"name":"getElectionAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"}]');
 
         // from https://tools.ietf.org/html/rfc3526#page-3
         // hex value of safe prime
         this.pHex = 'ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746c08ca18217c32905e462e36ce3be39e772c180e86039b2783a2ec07a28fb5c55df06f4c52c9de2bcbf6955817183995497cea956ae515d2261898fa051015728e5a8aacaa68ffffffffffffffff';
-        this.gHex = '2';
-        this.pBigNum = new BigInteger(this.pHex, 16);
-        this.gBigNum = new BigInteger('2');
-        this.contract = null;
+        this.pBigNum = new BigInteger(this.pHex, 16); // BigInteger instance of prime
+        this.gBigNum = new BigInteger('2'); // BigInteger instance of generator 2
+        this.contract = null; // connection to ElectionCreatorContract
     }
 
+    // connect to ElectionCreatorContract
     async connect() {
         try {
             this.contract = await new this.web3.eth.Contract(this.abi, this.address);
@@ -21,6 +24,7 @@ class ElectionCreatorHandler {
         }
     }
 
+    // Solidity wants 0x before hex strings, also standardize so each is 512 hex units long
     prepend0x(input) {
         let newInput = input;
         while(newInput.length < 512) {
@@ -30,6 +34,7 @@ class ElectionCreatorHandler {
         return newInput
     }
 
+    // create an election with given parameters
     async create(electionName, candidates, voters, account) {
         if (this.contract == null) {
             throw 'Contract yet connected';
@@ -43,6 +48,7 @@ class ElectionCreatorHandler {
         return electionAddress;
     }
 
+    // Return values for prime p and generator g
     getPandG() {
         return [this.pBigNum.toString(), this.gBigNum.toString()];
     }

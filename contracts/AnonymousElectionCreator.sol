20410111dd2b94ca30c5fbee5d01c5ea302596ec
robertagora <46801006+robertagora@users.noreply.github.com>
2022-06-30 16:04:09 -0400

    Update AnonymousElectionCreator.sol

--- contracts/AnonymousElectionCreator.sol
+++ contracts/AnonymousElectionCreator.sol
@@ -3,49 +3,43 @@ pragma solidity >=0.8.0 <0.9.0;
 import "./AnonymousElection.sol";
 
 contract AnonymousElectionCreator {
-    // who is the owner of this election creator
-    address private owner;
-    
-    mapping(string => address) private electionsMapping; // maps names to election address
-    string[] private electionsList; // list of names of elections
-    
+    // Who is the owner of this election creator?
+    // TODO: instantiate the address of the owner of the election.
+
+    // TODO: create a mapping of the election name string to the election address.
+    // TODO: create an array of strings of the names of elections.
+
+    // Create the constructor.
     constructor() {
-        owner = msg.sender;
-        electionsList = new string[](0);
+        // TO DO: instantiate the "owner" as the msg.sender.
+        // TO DO: instantiate the election list.
     }
-    
+
+
+    // Write the function that creates the election:
     function createElection(string memory _electionName, string[] memory _candidates, address[] memory _voters, bytes memory _p, bytes memory _g) public returns(address) {
         // make sure that the _electionName is unique
-        require(electionsMapping[_electionName] == address(0), "Election name not unique. An election already exists with that name");
-        require(_candidates.length > 1 && _voters.length > 0, "candidate list and voter list both need to have non-zero length, >1 candidate");
-        
-        // require none of the candidates are the empty string.
-        for (uint256 i = 0; i < _candidates.length; i++) {
-            require(bytes(_candidates[i]).length != 0, "candidate cannot be empty string");
-        }
-        
-        // create election
-        AnonymousElection election = new AnonymousElection(_candidates, _voters, _p, _g, msg.sender, _electionName);
-        
-        // create mapping between _electionName and election address
-        electionsMapping[_electionName] = address(election);
-        
-        // add name to electionsList
-        electionsList.push(_electionName);
-        
-        // return the address of the election created
-        return address(election);
+        // TODO: use the solidity require function to ensure the election name is unique. "Election name not unique. An election already exists with that name."
+        // TODO: use the solidity require function to ensure "candidate list and voter list both need to have non-zero length, >1 candidate."
+
+        // TODO: Using a for loop, require none of the candidates are the empty string.
+
+        // TODO: Create a new election.
+
+        // TODO: Create a mapping between _electionName and election address.
+
+        // TODO: Use .push() to add name to electionsList
+
+        // TODO: return the address of the election created
     }
-    
+
     // return address of an election given the election's name
     function getElectionAddress(string memory _electionName) public view returns(address) {
-        // ensure that _electionName is a valid election
-        require(electionsMapping[_electionName] != address(0));
-        
-        // return the address of requested election
-        return electionsMapping[_electionName];
+        // TODO: Using the solidity require function, ensure that _electionName is a valid election.
+
+        // TODO: Return the address of requested election.
     }
-    
+
     // return list of all election names created with this election creator
     function getAllElections() public view returns (string[] memory){
         return electionsList;
